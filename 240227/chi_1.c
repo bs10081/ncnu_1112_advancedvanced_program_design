e91369b6c65b56515d574d564a059188c1b2b66e
bs10081 <bs10081@bs10081.com>
2024-02-27 11:31:24 +0800

    Add chi_1.c and hello_world.c files

---
+++ 240227/chi_1.c
@@ -0,0 +1,13 @@
+/* 程式名稱：chi_1.c */
+/* 第一次體驗 */
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+    int i;
+
+    i = 1;
+    printf("C 程式練習\n");
+    printf("第一次體驗\n");
+    printf("i = %d\n", i);
+}
